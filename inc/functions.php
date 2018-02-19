261653b8ed76d52b4bedaf8bd6a78bdea1b293ad
Emmanuel Guido <eguido@csumb.edu>
2018-02-19 19:50:00 +0000

    Created functions file and added the existing php functions into it

---
+++ inc/functions.php
@@ -0,0 +1,53 @@
+<?php
+    function play()
+    {
+        $allPlayers = playerArray();
+        printGameStats($allPlayers);
+    }
+    
+    function playerArray()
+    {
+        $player1= array(
+            'name'=> 'Luis',
+            'imgURL' => './img/user_pics/player1.jpg',
+            'hand' => array(),
+            'points'=> 0
+        );
+        $player2 = array(
+            'name' => 'Emmanuel',
+            'imgURL' => './img/user_pics/player2.png',
+            'hands' => array(),
+            'points' => 0
+            );
+        
+        $player3 = array(
+            'name' => 'Adrian',
+            'imgURL' => './img/user_pics/player3.jpg',
+            'hand' => array(),
+            'points' => 0
+            );
+         $player4 = array(
+            'name' => 'Genesis',
+            'imgURL' => './img/user_pics/player4.jpg',
+            'hand' => array(),
+            'points' => 0
+            );
+        
+        $allPlayers = array(
+            $player1,
+            $player2,
+            $player3, 
+            $player4
+            );
+        return $allPlayers;
+    }
+    
+    function printGameStats($allPlayers) 
+    {
+        foreach ($allPlayers as $player) 
+        {
+            echo "<img src= '".$player['imgURL']."'/>";
+            echo $player['name']."<br/>";
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,53 +1,19 @@
+<?php
+include './inc/functions.php';
+?>
+
 <!DOCTYPE html>
 <html>
-      
     <head>
-   
+        <title> Silver Jack </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     
     <body>
         <?php 
-        
-        $player1= array(
-            'name'=> 'Luis',
-            'imgURL' => './img/user_pics/player1.jpg',
-            'hand' => array(),
-            'points'=> 0
-        );
-        $player2 = array(
-            'name' => 'Emmanuel',
-            'imgURL' => './img/user_pics/player2.png',
-            'hands' => array(),
-            'points' => 0
-            );
-        
-        $player3 = array(
-            'name' => 'Adrian',
-            'imgURL' => './img/user_pics/player3.jpg',
-            'hand' => array(),
-            'points' => 0
-            );
-         $player4 = array(
-            'name' => 'Genesis',
-            'imgURL' => './img/user_pics/player4.jpg',
-            'hand' => array(),
-            'points' => 0
-            );
-        
-        $allPlayers = array(
-            $player1,
-            $player2,
-            $player3
-            );
-            
-            function printGameStats($allPlayers) {
-                foreach ($allPlayers as $player) {
-                    echo "<img src= '".$player['imgURL']."'/>";
-                    echo $player['name']."<br/>";
-                }
-            }
-            
-        printGameStats($allPlayers);
+            play();
         ?>
     </body>
 </html>
